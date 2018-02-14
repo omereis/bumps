5afeafdd3ec7bbead856e9539d636f86158ae0e8
Omer Eisenberg <omereis@yahoo.com>
2018-02-14 12:01:55 -0500

    modified heading color, batch file

--- extra/bumps_flask/run_bumps.bat
+++ extra/bumps_flask/run_bumps.bat
@@ -7,6 +7,6 @@ docker build . -t bumps
 docker run --name bumps_redis -d redis
 rem docker run --link bumps_redis -p 5000:5000 bumps
 docker run -p 5000:5000 --link bumps_redis -d --name bumps_gui bumps
-docker run -it -h ernie giovtorres/docker-centos7-slurm:latest
+rem docker run -it -h ernie giovtorres/docker-centos7-slurm:latest
 docker exec -i -t bumps_gui "bash"
 
