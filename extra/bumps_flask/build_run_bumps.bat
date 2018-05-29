8c6e6285b205f8af9a78f2974a76869725e809f3
Omer Eisenberg <omereis@yahoo.com>
2018-05-29 15:59:07 -0400

    worker runs automaticly, using ENTRYPOINT

--- extra/bumps_flask/build_run_bumps.bat
+++ extra/bumps_flask/build_run_bumps.bat
@@ -5,7 +5,7 @@ rem docker run --name bumps_redis -d redis
 docker run --name redis-server -d redis
 rem docker run -d --link bumps_redis -p 5000:5000 bumps_gui
 docker run -d -e RABBITMQ_NODENAME=my-rabbit -p 22 --name rabbit-server rabbitmq
-docker run -d --link redis-server -e REDIS_SERVER="redis-server" --link  rabbit-server -p 5000:5000 --name oe_bumps oe_bumps
+rem docker run -d --link redis-server -e REDIS_SERVER="redis-server" --link  rabbit-server -p 5000:5000 --name oe_bumps oe_bumps
 docker run -d --link redis-server --link rabbit-server -e "BACKEND_SERVER=redis-server" -p 5000:5000 --name oe_bumps oe_bumps
 
 
