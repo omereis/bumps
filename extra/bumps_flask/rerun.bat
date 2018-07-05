352dc0a738f247b83c1fc277081b918401719ac5
Omer Eisenberg <omereis@yahoo.com>
2018-07-05 15:14:31 -0400

    added base build command

--- extra/bumps_flask/rerun.bat
+++ extra/bumps_flask/rerun.bat
@@ -1,4 +1,5 @@
 docker container rm -f oe_bumps
+docker build --rm -t bumps_base -f Dockerfile.bumps .
 docker build --rm -t oe_bumps -f Dockerfile_bumps.oe .
 rem docker run --link redis-server --link rabbit-server -e "BACKEND_SERVER=redis-server" -p 5000:5000 -h oe_bumps --name oe_bumps oe_bumps
 docker run -d -it --link redis-server --link rabbit-server -e "BACKEND_SERVER=redis-server" -p 5000:5000 -h oe_bumps --name oe_bumps oe_bumps
