e399431a99b775c088a05546e597ee839656400f
Omer Eisenberg <omereis@yahoo.com>
2018-07-19 14:05:46 -0400

    new entry working

--- extra/bumps_flask/bumps_run.bat
+++ extra/bumps_flask/bumps_run.bat
@@ -3,3 +3,4 @@ docker build --rm -t bumps_gui -f Dockerfile.bumps .
 docker run -d -it --link redis-server --link rabbit-server -e "BACKEND_SERVER=ncnr-r9nano" -e "BROKER_SERVER=ncnr-r9nano" -p 5000:5000 -h bumps_gui --name bumps_gui bumps_gui
 docker exec -i -t bumps_gui "bash"
 
+rem docker run --link redis-server --link rabbit-server -e "BACKEND_SERVER=ncnr-r9nano" -e "BROKER_SERVER=ncnr-r9nano" -p 5000:5000 -h bumps_gui --name bumps_gui bumps_gui
