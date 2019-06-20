42933be9d5d686a3193ae2bcd31a0411ca8b0085
omereis <omereis@yahoo.com>
2019-06-20 17:01:14 -0400

    daily

--- extra/bumps_flask/gui_run.sh
+++ extra/bumps_flask/gui_run.sh
@@ -1,2 +1,1 @@
-flask run --host 0.0.0.0 --port 4000 &
-cd bumps_server && python bumps_ws_server.py --host 0.0.0.0 --port 4567 &
+cd bumps_server && python bumps_ws_server.py --host 0.0.0.0 --port 4567
