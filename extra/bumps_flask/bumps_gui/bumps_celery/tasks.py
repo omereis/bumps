0d4a778ab23c954e748ebcb9d56b11d66d925087
omereis <omereis@yahoo.com>
2019-08-06 12:05:52 -0400

    removed unused celery functions, that were put for development

--- extra/bumps_flask/bumps_gui/bumps_celery/tasks.py
+++ extra/bumps_flask/bumps_gui/bumps_celery/tasks.py
@@ -9,48 +9,10 @@ from .oe_debug import print_debug
 #------------------------------------------------------------------------------
 @app.task   
 def run_bumps(message):
-#def run_bumps(client_message):
-    #json_message = json.loads(message.replace('\n','\\n'))
-    #print(f'json message: {json_message}')
-    #res = run_local_bumps (json_message)
     res = run_local_bumps (message)
     return res
 #------------------------------------------------------------------------------
 @app.task
 def add(x, y):
     return x + y
-#------------------------------------------------------------------------------
-@app.task
-def write_file(file_name,file_content):
-    try:
-        f = open (file_name, 'w')
-        f.write(file_content)
-        f.close()
-        res = f'file {file_name} saved'
-    except Exception as e:
-        res = f'{e}'
-    return (res)
-#------------------------------------------------------------------------------
-@app.task
-def read_bin(file_name):
-    try:
-        f = open (file_name, 'rb')
-        bin_content = f.read()
-        f.close()
-        file_content = bin_content.hex()
-        #res = f'file {file_name} saved'
-    except Exception as e:
-        res = f'{e}'
-    return (file_content)
-#------------------------------------------------------------------------------
-@app.task
-def read_alpha(file_name):
-    try:
-        f = open (file_name, 'r')
-        file_content = f.read()
-        f.close()
-        #res = f'file {file_name} saved'
-    except Exception as e:
-        res = f'{e}'
-    return (file_content)
 #------------------------------------------------------------------------------
