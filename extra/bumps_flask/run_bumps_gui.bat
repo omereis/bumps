5da8049f3ec490e6d04f229626683a32f06815a3
omereis <omereis@yahoo.com>
2019-08-07 16:25:22 -0400

    removed rem (commented) lines

--- extra/bumps_flask/run_bumps_gui.bat
+++ extra/bumps_flask/run_bumps_gui.bat
@@ -1,6 +1,5 @@
 docker container rm -f bumps_gui
 docker build --rm -f bumps_gui.dockerfile -t bumps_gui .
-docker run -it -d --name bumps_gui --link redis-server -p 5672 --link rabbit-server -p 4000:4000 -p 4567:4567 bumps_gui
+docker run -it -d -h bumps_host --name bumps_gui -p 5672 -p 4000:4000 -p 4567:4567 bumps_gui
+rem docker run -it -d -h bumps_host --name bumps_gui --link redis-server -p 5672 --link rabbit-server -p 4000:4000 -p 4567:4567 bumps_gui
 docker exec -i -t bumps_gui bash
-rem docker run -d --name bumps_gui -p 8765:8765 -p 4000:4000 -p 4567:4567 bumps_gui
-rem docker exec -i -t bumps_redis "redis-cli"
