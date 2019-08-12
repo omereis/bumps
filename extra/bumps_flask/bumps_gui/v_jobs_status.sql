2756665d4edf328153c7866ef07bc837b4ab52ce
omereis <omereis@yahoo.com>
2019-08-12 10:01:56 -0400

    added tag to the view

--- extra/bumps_flask/bumps_gui/v_jobs_status.sql
+++ extra/bumps_flask/bumps_gui/v_jobs_status.sql
@@ -10,10 +10,11 @@ CREATE VIEW v_bumps_jobs_status (
 	end_time,
 	problem_file,
 	status_time,
-	status_name 
+	status_name ,
+	tag
 )
 AS
-	SELECT t_bumps_jobs.job_id AS 'job_id',sent_ip,sent_time,message,results_dir,job_status,end_time,problem_file,status_time,status_name FROM t_bumps_jobs,t_jobs_status WHERE t_bumps_jobs.job_id=t_jobs_status.job_id
+	SELECT t_bumps_jobs.job_id AS 'job_id',sent_ip,sent_time,message,results_dir,job_status,end_time,problem_file,status_time,status_name,tag FROM t_bumps_jobs,t_jobs_status WHERE t_bumps_jobs.job_id=t_jobs_status.job_id
 ;
 
 /*
