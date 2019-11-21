a9171cf989848e0e0f5338adb4176f97ee4d0b3a
unknown <omereis@yahoo.com>
2019-11-21 22:24:42 +0200

    simpler redis, rabbitmq pull & run

--- extra/bumps_flask/servers_run.bat
+++ extra/bumps_flask/servers_run.bat
@@ -1,9 +1,5 @@
 docker container rm -f redis-server
 docker container rm -f rabbit-server
 
-docker pull rabbitmq:latest
-docker pull redis:latest
-
-
-docker run -d --name redis-server redis
-docker run -d --name rabbit-server rabbitmq
+docker run -d --name redis-server redis:latest
+docker run -d --name rabbit-server rabbitmq:latest
