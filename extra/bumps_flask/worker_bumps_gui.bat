1fbee8a75b0a40a6a3810f0ea04f5fd42c673356
unknown <omereis@yahoo.com>
2019-12-07 15:38:05 +0200

    running without RUN_WS environment, which makes it a worker

--- extra/bumps_flask/worker_bumps_gui.bat
+++ extra/bumps_flask/worker_bumps_gui.bat
@@ -1,7 +1,3 @@
 docker container rm -f bumps_worker
-rem docker build --rm -f bumps_gui.dockerfile -t bumps_gui .
-docker run -it -d --name bumps_worker --link redis-server --link rabbit-server bumps_gui
-docker run -it -d -h bumps_worker --name bumps_worker bumps_gui
-docker exec -i -t bumps_worker bash
-rem docker run -d --name bumps_gui -p 8765:8765 -p 4000:4000 -p 4567:4567 bumps_gui
-rem docker exec -i -t bumps_redis "redis-cli"
+
+docker run -it -d --link bumps_db --link redis-server --link rabbit-server --name bumps_worker -e "DATABASE_SERVER=%1" bumps_gui
