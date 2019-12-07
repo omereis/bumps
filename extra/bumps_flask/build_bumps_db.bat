6445f21537b0a9d11c8f525ceaa9fd9f1bdccbe8
unknown <omereis@yahoo.com>
2019-12-07 15:49:53 +0200

    removed running interactive bash inside the container

--- extra/bumps_flask/build_bumps_db.bat
+++ extra/bumps_flask/build_bumps_db.bat
@@ -1,4 +1,4 @@
 docker container rm -f bumps_db
 docker build --rm -f bumps_db.dockerfile -t bumps_db .
 docker run -it -d -h bumps_db_host --name bumps_db --env MYSQL_ROOT_PASSWORD=bumps_root -p 5306:3306 bumps_db
-docker exec -i -t bumps_db bash
+rem docker exec -i -t bumps_db bash
