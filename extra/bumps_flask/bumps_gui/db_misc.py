c7ae5f8f1c1d04643c731c7b99bd05def163bf8c
unknown <omereis@yahoo.com>
2020-01-25 17:49:00 +0200

    added sqlite procedure. Not used in current version

--- extra/bumps_flask/bumps_gui/db_misc.py
+++ extra/bumps_flask/bumps_gui/db_misc.py
@@ -17,19 +17,36 @@ def get_next_job_id(connection):
         job_id = 0
     job_id += 1
     return job_id
-#------------------------------------------------------------------------------
-def results_dir_for_job (database_engine, job_id):
+##------------------------------------------------------------------------------
+def get_sqlite_next_job_id(connection):
+    job_id = 0
     try:
-        connection = database_engine.connect()
+        sql = f'select max({fld_JobID}) from {tbl_bumps_jobs};'
+        cur = connection.cursor()
+        res = cur.execute(sql)
+        for row in res:
+            db_job_id = row.values()[0]
+            #db_job_id = row[0]
+        if db_job_id:
+            job_id = db_job_id
+    except Exception as e:
+        print(f"Error in get_next_job_id: {e}")
+        job_id = 0
+    job_id += 1
+    return job_id
+#------------------------------------------------------------------------------
+def results_dir_for_job (db_connection, job_id):
+    try:
+        #connection = database_engine.connect()
         strSql = f'select {fld_ResultsDir} from {tbl_bumps_jobs} where {fld_JobID}={job_id};'
-        res = connection.execute(strSql)
+        res = db_connection.execute(strSql)
         for row in res:
             res_dir = f'{row[0]}'
     except Exception as e:
         print(f'Error {e}')
         res_dir = None
     finally:
-        connection.close()
+        db_connection.close()
     return res_dir
 #------------------------------------------------------------------------------
 def get_problem_file_name (database_engine, job_id):
